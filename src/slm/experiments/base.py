5b3cb1635320c75b117f19b3f214bc5a97779bc3
tedasdf <154312357+tedasdf@users.noreply.github.com>
2026-04-04 10:34:17 +1100

    bug: base experiment import

--- src/slm/experiments/base.py
+++ src/slm/experiments/base.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Any
 
-from slm.training.run_config import RunConfig
+from training.run_config import RunConfig
 
 
 class BaseExperiment(ABC):
