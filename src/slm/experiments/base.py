4fe74b9bb9a900ef7465688790ac80c42f6523a7
tedasdf <154312357+tedasdf@users.noreply.github.com>
2026-05-17 15:01:56 +1000

    Update base.py

--- src/slm/experiments/base.py
+++ src/slm/experiments/base.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Any
 
-from slm.training import RunConfig
+from training import RunConfig
 
 
 class BaseExperiment(ABC):
